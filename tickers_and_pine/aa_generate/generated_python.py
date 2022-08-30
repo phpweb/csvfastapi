e7a255b68a4ec7dd9a13f877fe457aac8036ce3c
Ramazan Gezer <r.gezer@reply.de>
2022-08-31 00:17:32 +0200

    all is working, only syms, disable auto deploy

--- tickers_and_pine/aa_generate/generated_python.py
+++ tickers_and_pine/aa_generate/generated_python.py
@@ -19,14 +19,12 @@ push_s_array = '// Add Symbols \n'
 push_u_array = '// FLAGS \n'
 # CLOSE
 current_price_arr = '// Current price \n'
-trend_arr = '// Buy continuation \n'
-trend_minus_one_arr = '// Baseline BUY alert condition \n'
-trendhtf_arr = '// Baseline SELL alert condition \n'
-trendhtf_minus_one_arr = '// Condition for plotting \n'
+trend_arr = '// Super trend \n'
+trend_minus_one_arr = '// Super trend minus one \n'
+trendhtf_arr = '// trendhtf \n'
+trendhtf_minus_one_arr = '// trendhtf minus one \n'
 
-# arrays to create the type to push later
-
-str_test_dict = {}
+symbols_dict = {}
 u_array_dict = {}
 security_calls_dict = {}
 push_s_array_dict = {}
@@ -45,30 +43,24 @@ for x in range(read_csv.size):
     # cnt_str = str(j) + counter_str
     cnt_str = counter_str
     # symbol string
-    symbols_array += 's' + cnt_str + " = input.symbol('" + read_csv.values[x][0] \
-                     + "', group = 'Symbols', inline = '" + 's' + cnt_str + "')\n"
-    str_test_dict[j] = symbols_array
-
+    symbols_array += 's' + cnt_str + " = input.symbol('" + read_csv.values[x][0] + "', group = 'Symbols', inline = '" + 's' + cnt_str + "')\n"
+    symbols_dict[j] = symbols_array
     # u symbol string
-    u_array_str += 'u' + cnt_str + " = input.bool(true,  title = \"\", group = 'Symbols', inline = 's" + cnt_str + "')\n"
+    u_array_str += 'u' + cnt_str + " = input.bool(true,  title = '', group = 'Symbols', inline = 's" + cnt_str + "')\n"
     u_array_dict[j] = u_array_str
     # security calls
-    security_calls += f"[current_price{cnt_str}, Trend{cnt_str}, TrendMinusOne{cnt_str}, TrendHtf{cnt_str}, TrendHtfMinusOne{cnt_str}] = " \
-                      f"request.security(s{cnt_str}, timeframe.period, screener_func())\n"
-
+    security_calls += f"[current_price{cnt_str}, trend{cnt_str}, trend_minus_one{cnt_str}, trendhtf{cnt_str}, trendhtf_minus_one{cnt_str}] = request.security(s{cnt_str}, timeframe.period, screener_func())\n"
     security_calls_dict[j] = security_calls
-    # crate arrays to push
+    
     arrays_to_push1 = f'\n/////////////\n// ARRAYS //\n\n'
     arrays_to_push1 += f"s_arr   = array.new_string(0)\n"
     arrays_to_push2 = f"u_arr   = array.new_bool(0)\n"
     arrays_to_push3 = f"current_price_arr   = array.new_float(0)\n"
-    arrays_to_push4 = f"trend_arr   = array.new_bool(0)\n"
-    arrays_to_push5 = f"trend_minus_one_arr   = array.new_bool(0)\n"
-    arrays_to_push6 = f"trendhtf_arr   = array.new_bool(0)\n"
-    arrays_to_push7 = f"trendhtf_minus_one_arr   = array.new_bool(0)\n"
-    arrays_to_push_dict[
-        j] = arrays_to_push1 + arrays_to_push2 + arrays_to_push3 + arrays_to_push4 + arrays_to_push5 + arrays_to_push6 + arrays_to_push7
-
+    arrays_to_push4 = "trend_arr = array.new_int(0) \n"
+    arrays_to_push5 = "trend_minus_one_arr = array.new_int(0) \n"
+    arrays_to_push6 = "trendhtf_arr = array.new_int(0) \n"
+    arrays_to_push7 = "trendhtf_minus_one_arr = array.new_int(0) \n"
+    arrays_to_push_dict[j] = arrays_to_push1 + arrays_to_push2 + arrays_to_push3 + arrays_to_push4 + arrays_to_push5 + arrays_to_push6 + arrays_to_push7 
     # Add Symbols
     push_s_array += f"array.push(s_arr, only_symbol(s{cnt_str}))\n"
     push_s_array_dict[j] = push_s_array
@@ -78,54 +70,41 @@ for x in range(read_csv.size):
     # Close price (current price)
     current_price_arr += f"array.push(current_price_arr, current_price{cnt_str})\n"
     push_cl_array_dict[j] = current_price_arr
-    # Buy continuation
-    trend_arr += f"array.push(trend_arr, Trend{cnt_str})\n"
+    
+    trend_arr += f"array.push(trend_arr, trend{cnt_str})\n"
     trend_dict[j] = trend_arr
 
-    trend_minus_one_arr += f"array.push(trend_minus_one_arr, TrendMinusOne{cnt_str})\n"
+    trend_minus_one_arr += f"array.push(trend_minus_one_arr, trend_minus_one{cnt_str})\n"
     trend_minus_one_dict[j] = trend_minus_one_arr
 
-    trendhtf_arr += f"array.push(trendhtf_arr, TrendHtf{cnt_str})\n"
+    trendhtf_arr += f"array.push(trendhtf_arr, trendhtf{cnt_str})\n"
     trendhtf_dict[j] = trendhtf_arr
 
-    trendhtf_minus_one_arr += f"array.push(trendhtf_minus_one_arr, TrendHtfMinusOne{cnt_str})\n"
+    trendhtf_minus_one_arr += f"array.push(trendhtf_minus_one_arr, trendhtf_minus_one{cnt_str})\n"
     trendhtf_minus_one_dict[j] = trendhtf_minus_one_arr
+
 
     if i % max_items == 0:
         j += 1
-        # arrays
-        # str_test += f'\n// Part {str(j)}\n\n'
-        # make them empty after 40
-        symbols_array = f'// SYMBOLS //\n// Part {str(j)}\n\n'
-        u_array_str = f'/////////////\n// SYMBOLS //\n// Part {str(j)}\n\n'
-        security_calls = f'// Security calls \n//Part {str(j)}\n\n'
-        push_s_array = '// Add Symbols \n'
-        push_u_array = '// FLAGS \n'
-        current_price_arr = '// Current price \n'
-        trend_arr = '// Supertrend \n'
-        trend_minus_one_arr = '// Supertrend minus one \n'
-        trendhtf_arr = '// Trend multiple timeframe \n'
-        trendhtf_minus_one_arr = '// Trend multiple timeframe minus one \n'
-
         i = 0
     i += 1
-
 calculations = ''
-with open('functions_ssl_hybrid.txt') as f:
+with open('functions.txt') as f:
     calculations += f.read()
 
 if_condition_text = ''
-with open('if_conditions_ssl_hybrid.txt') as f:
+with open('if_conditions.txt') as f:
     if_condition_text += f.read()
 
 for k in range(1, j + 1):
     # pine script
     pine_script = f'//@version=5\nindicator("Benimki Supertrend MTF Heikin Ashi Part_{str(k)}", overlay=true)\n\n'
+
     with open(f'output/pine_part_{k}.txt', 'w') as f:
         f.write(pine_script)
         f.write(calculations)
         f.write(u_array_dict[k])
-        f.write(str_test_dict[k])
+        f.write(symbols_dict[k])
         f.write(security_calls_dict[k])
         f.write(arrays_to_push_dict[k])
         f.write(push_s_array_dict[k])
@@ -135,5 +114,4 @@ for k in range(1, j + 1):
         f.write(trend_minus_one_dict[k])
         f.write(trendhtf_dict[k])
         f.write(trendhtf_minus_one_dict[k])
-        # f.write(if_condition_part)
         f.write(if_condition_text)
---
+++ tickers_and_pine/aa_generate/generete_tv_pinescript.py
@@ -0,0 +1,144 @@
+# how many conditions, excepts close (price)
+indicator_name = 'Benimki Supertrend MTF Heikin Ashi'
+conditions = {
+    'trend': '// Super trend \\n',
+    'trend_minus_one': '// Super trend minus one \\n',
+    'trendhtf': '// trendhtf \\n',
+    'trendhtf_minus_one': '// trendhtf minus one \\n'
+}
+
+conditions_type = {
+    'trend': 'array.new_int(0) \\n',
+    'trend_minus_one': 'array.new_int(0) \\n',
+    'trendhtf': 'array.new_int(0) \\n',
+    'trendhtf_minus_one': 'array.new_int(0) \\n'
+}
+
+
+def generate_arrays():
+    array_items = '\n'
+    array_items_dict = '\n'
+    security_calls = ''
+    conditions_array_push = '\n'
+    pushing_array_arrays = '\n'
+    output_text = '\n'
+    i = 4
+    for key, value in conditions.items():
+        array_items += f'{key}_arr = \'' + value + '\'\n'
+        array_items_dict += f'{key}_dict = ' + '{}\n'
+        security_calls += f', {key}' + '{cnt_str}'
+        conditions_array_push += f'    arrays_to_push{i} = \"{key}_arr = ' + conditions_type[key] + '\"\n'
+        pushing_array_arrays += f'    {key}_arr += f"array.push({key}_arr, {key}' + '{cnt_str}' + f')\\n"\n    {key}_dict[j] = {key}_arr\n\n'
+        output_text += f'        f.write({key}_dict[k])\n'
+        i += 1
+    standard_array_items = '''
+symbols_dict = {}
+u_array_dict = {}
+security_calls_dict = {}
+push_s_array_dict = {}
+push_u_array_dict = {}
+push_cl_array_dict = {}
+arrays_to_push_dict = {}'''
+    if_condition_part_1 = '''
+for x in range(read_csv.size):
+    counter_str = str(i)
+    if i < 10:
+        counter_str = '0' + counter_str
+    # cnt_str = str(j) + counter_str
+    cnt_str = counter_str
+    # symbol string
+    symbols_array += 's' + cnt_str + " = input.symbol('" + read_csv.values[x][0] + "', group = 'Symbols', inline = '" + 's' + cnt_str + "')\\n"
+    symbols_dict[j] = symbols_array
+    # u symbol string
+    u_array_str += 'u' + cnt_str + " = input.bool(true,  title = '', group = 'Symbols', inline = 's" + cnt_str + "')\\n"
+    u_array_dict[j] = u_array_str
+    # security calls
+    security_calls += f"[current_price{cnt_str}{other_elements}] = request.security(s{cnt_str}, timeframe.period, screener_func())\\n"
+    security_calls_dict[j] = security_calls
+    '''.format(other_elements=security_calls, cnt_str='{cnt_str}')
+    arrays_push_standard = '''
+    arrays_to_push1 = f'\\n/////////////\\n// ARRAYS //\\n\\n'
+    arrays_to_push1 += f"s_arr   = array.new_string(0)\\n"
+    arrays_to_push2 = f"u_arr   = array.new_bool(0)\\n"
+    arrays_to_push3 = f"current_price_arr   = array.new_float(0)\\n"'''
+    all_arrays_push = '    arrays_to_push_dict[j] = '
+    for i in range(1, len(conditions) + 4):
+        all_arrays_push += f'arrays_to_push{i} + '
+    all_arrays_push = all_arrays_push[:-2]
+
+    push_arrays_standard = '''
+    # Add Symbols
+    push_s_array += f"array.push(s_arr, only_symbol(s{cnt_str}))\\n"
+    push_s_array_dict[j] = push_s_array
+    # Flags
+    push_u_array += f"array.push(u_arr, u{cnt_str})\\n"
+    push_u_array_dict[j] = push_u_array
+    # Close price (current price)
+    current_price_arr += f"array.push(current_price_arr, current_price{cnt_str})\\n"
+    push_cl_array_dict[j] = current_price_arr
+    '''
+    some_if_condition = '''
+    if i % max_items == 0:
+        j += 1
+        i = 0
+    i += 1
+calculations = ''
+with open('functions.txt') as f:
+    calculations += f.read()
+
+if_condition_text = ''
+with open('if_conditions.txt') as f:
+    if_condition_text += f.read()
+
+for k in range(1, j + 1):
+    # pine script'''
+
+    some_if_condition += '''
+    pine_script = f'//@version=5\\nindicator("{script_name} Part_{strk}", overlay=true)\\n\\n'
+'''.format(script_name=indicator_name, strk='{str(k)}')
+
+    some_if_condition += '''
+    with open(f'output/pine_part_{k}.txt', 'w') as f:
+        f.write(pine_script)
+        f.write(calculations)
+        f.write(u_array_dict[k])
+        f.write(symbols_dict[k])
+        f.write(security_calls_dict[k])
+        f.write(arrays_to_push_dict[k])
+        f.write(push_s_array_dict[k])
+        f.write(push_u_array_dict[k])
+        f.write(push_cl_array_dict[k])'''
+
+    output_text += '        f.write(if_condition_text)'
+
+    headline_part = ''
+    with open('head_part_1.txt') as f:
+        headline_part += f.read()
+    with open('generated_python.py', 'w') as f:
+        f.write(headline_part)
+        f.write(array_items)
+        f.write(standard_array_items)
+        f.write(array_items_dict)
+        f.write(if_condition_part_1)
+        f.write(arrays_push_standard)
+        f.write(conditions_array_push)
+        f.write(all_arrays_push)
+        f.write(push_arrays_standard)
+        f.write(pushing_array_arrays)
+        f.write(some_if_condition)
+        f.write(output_text)
+    print(headline_part)
+    print(array_items)
+    print(standard_array_items)
+    print(array_items_dict)
+    print(if_condition_part_1)
+    print(arrays_push_standard)
+    print(conditions_array_push)
+    print(all_arrays_push)
+    print(push_arrays_standard)
+    print(pushing_array_arrays)
+    print(some_if_condition)
+    print(output_text)
+
+
+generate_arrays()
